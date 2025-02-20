a8d89f64ea69c5b0ef5bb231c8dee37da407b842
Liza21l <Liza2009koshenya@gmail.com>
2025-02-20 16:34:37 +0200

    update

--- text.py
+++ text.py
@@ -16,10 +16,30 @@ curse_currency = {
     "USD/PLN": 3.88 
 }
 
+financial_records = {
+    #прибуток   
+    "profit": [],
+    #Витрати
+    "costs": [],
+    #інвестиції
+    "investment": [],
+
+}
+
 def convert(amount, from_currency, to_currency):
     key = from_currency.upper() + "/" + to_currency.upper()
     if key in curse_currency:
-        return amount * curse_currency[key]
+        converted_amount = amount * curse_currency[key]
+        if from_currency in wallet and wallet[from_currency] >= amount:
+            wallet[from_currency] -= amount
+            if to_currency in wallet:
+                wallet[to_currency] += converted_amount
+            else:
+                wallet[to_currency] = converted_amount
+            return converted_amount
+        else:
+            print("Недостатньо коштів або у вас немає цієї валюти")
+            return None
     else:
         return None
 
@@ -52,6 +72,17 @@ def curse():
     for currency in curse_currency:
         print(currency, ":", curse_currency[currency])
 
+def share_financial_record():
+    for type in financial_records:
+        print(type, ":", financial_records[type])
+def add_financial_record(type, amount, currency, description):
+    if type in financial_records:
+        financial_records[type].append((amount, description))
+        if type == "profit":
+            replenish_wallet(currency, amount)
+        if type == "costs" or type == "investment":
+            withdrawal_funds(currency, amount)
+
 while True:
     print("____Purr-Currency🐾💵____")
     print(f"{Fore.CYAN}1.{Style.RESET_ALL} Показати баланс")
@@ -59,7 +90,9 @@ while True:
     print(f"{Fore.RED}3.{Style.RESET_ALL} Зняти кошти")
     print(f"{Fore.BLUE}4.{Style.RESET_ALL} Конвертувати валюту")
     print(f"{Fore.YELLOW}5.{Style.RESET_ALL} Курс валюти на даний момент")
-    print(f"{Fore.MAGENTA}6.{Style.RESET_ALL} Вийти")
+    print(f"{Fore.WHITE}6.{Style.RESET_ALL} фінансові записи")
+    print(f"{Fore.RED}7.{Style.RESET_ALL} Огляд на записи ")
+    print(f"{Fore.MAGENTA}8.{Style.RESET_ALL} Вийти")
     
     action = input("Виберіть дію, яку ви хочете зробити, вписавши просто цифру: ")
 
@@ -86,6 +119,24 @@ while True:
     elif action == "5":
         curse()
     elif action == "6":
+        print("Додати прибуток")
+        print("Додати витрати")
+        print("Iнвестиції")
+        type = input("Виберіть дію вписавши на англійській profit, costs або investment: ")
+        currency = input("Введіть валюту(UAH) наприклад: ")
+        amount = float(input("Введіть суму: "))
+        description = input("Введіть опис: ")
+        if type == "profit":
+            add_financial_record("profit", amount, currency, description)
+        if type == "costs":
+            add_financial_record("costs", amount, currency, description)
+        if type == "investments":
+            add_financial_record("investments", amount, currency, description)
+        else:
+            print("Ви не вірно ввели, спробуйте ще раз")
+    elif action == "7":
+        share_financial_record()
+    elif action == "8":
         break
     else:
         print("Невірний вибір, спробуйте ще раз.")
